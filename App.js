effd2211d1c8e2690da132fe80cecfea64b91336
Remie <6835891+Nantris@users.noreply.github.com>
2024-12-04 17:13:30 -0500

    Switch to yarn and install theme-control package

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { setThemePreference } from '@vonovak/react-native-theme-control'
+
+setThemePreference('dark')
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button text="Create alert" onPress={() => Alert.alert('Hello world')} />
       <StatusBar style="auto" />
     </View>
   );
